1890b4acbcfb29315bb0e2a05b54a68c9a866310
Felipe <ofelipedbzgt@hotmail.com>
2019-06-20 18:45:03 -0300

    Part 2

--- venv/test.py
+++ venv/test.py
@@ -1,3 +1,28 @@
-from engine import main
+#!/usr/bin/env python
+import libtcodpy as tcod
 
-main()
+# ######################################################################
+# Global Game Settings
+# ######################################################################
+# Size of window
+SCREEN_WIDTH = 80  # characters wide
+SCREEN_HEIGHT = 50  # characters tall
+LIMIT_FPS = 20  # 20 frames-per-second maximum
+
+# Setup Font
+font_filename = 'arial10x10.png'
+tcod.console_set_custom_font(font_filename, tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
+
+# Initialize screen
+tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
+
+# Set FPS
+tcod.sys_set_fps(LIMIT_FPS)
+
+# ######################################################################
+# Game loop
+# ######################################################################
+while not tcod.console_is_window_closed():
+    tcod.console_set_default_foreground(0, tcod.white)
+    tcod.console_put_char(0, 1, 1, '@', tcod.BKGND_NONE)
+    tcod.console_flush()
